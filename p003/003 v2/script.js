f1f59868380c9f00c3f9b31a589cbf03a5553d98
Sayan Saruglar <s2a3ng@gmail.com>
2023-05-11 19:18:03 +0700

    Update v2
    Added adaptive designe and hamburger menu in mobile version

---
+++ p003/003 v2/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const menu = document.querySelector('.nav-menu'),
+    menuItem = document.querySelector('.nav-menu__list-item'),
+    hamburger = document.querySelector('.hamburger');
+    
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_acitve');
+        menu.classList.toggle('nav-menu_active');
+    });
+    
+    menuItem.forEach(item => {
+        item.addEventListener('click', ()=>{
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav-menu_active');
+        })
+    })
+
+})
+
